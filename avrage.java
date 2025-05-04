f138e0323587c099422942d721a00affc8188403
HARIOM VERMA <152371151+Hari06om@users.noreply.github.com>
2025-05-04 21:48:36 +0530

    Add files via upload
    average in java

---
+++ avrage.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class avrage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = sc.nextInt();
+        int z = a+b+c;
+        int av = z/3;
+        System.out.println(av);
+        
+    }
+}
